d23b4e5c0b857e62698c7e736e91dae956791550
Ibrahim Kirabo <kiraboibra268@gmail.com>
2025-11-15 11:16:12 +0300

    Add csrf trusted origins

--- triptonic_safaris_backend/settings.py
+++ triptonic_safaris_backend/settings.py
@@ -17,6 +17,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
+
 
 # -------------------------------------------------------------------
 # APPLICATIONS
